43e5ee734875099e71507bffbc489115069c723a
Dennis <dlh4fx@virginia.edu>
2016-09-14 16:43:09 -0400

    Initial Commit

---
+++ Things to Do/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Things to Do
+//
+//  Created by Dennis Huang on 9/14/16.
+//  Copyright © 2016 Dennis Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
